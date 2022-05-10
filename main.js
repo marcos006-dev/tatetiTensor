663f7a1b2fccb10b685fe0f42cc2751a7b42a4a8
Marcos Franco <marcosfrc013@gmail.com>
2022-05-10 16:20:49 -0300

    Primer commit

---
+++ main.js
@@ -0,0 +1,233 @@
+import * as tf from '@tensorflow/tfjs';
+
+const tablero = document.getElementById('tablero');
+const restartButton = document.getElementById('btnRestart');
+const btnPredecir = document.getElementById('btnPredecir');
+
+let currentPlayer = 1;
+let movesPlayed = 0;
+let isGameStarted = true;
+let modelo;
+let valoresTablero = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+// let tableroParaJugarO = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+function setCells() {
+  const cantidadCelda = 9;
+  const contenidoCelda = `
+        <div class="main__cell">
+            <p class= "main__cell--text"></p>
+        </div>
+    `;
+
+  let aux = '';
+  for (let i = 1; i <= cantidadCelda; i++) {
+    aux += contenidoCelda;
+  }
+
+  tablero.innerHTML = aux;
+}
+
+function restartGame() {
+  for (let cell of tablero.children) {
+    cell.children[0].innerHTML = '';
+  }
+  movesPlayed = 0;
+  currentPlayer = 1;
+  isGameStarted = true;
+}
+
+function checkMove(index) {
+  const children = tablero.children;
+
+  //Check Row
+  if (index >= 0 && index <= 2) {
+    if (
+      children[0].children[0].innerHTML === children[1].children[0].innerHTML &&
+      children[1].children[0].innerHTML === children[2].children[0].innerHTML &&
+      children[2].children[0].innerHTML ===
+        children[index].children[0].innerHTML
+    ) {
+      isGameStarted = false;
+    }
+  } else if (index >= 3 && index <= 5) {
+    if (
+      children[3].children[0].innerHTML === children[4].children[0].innerHTML &&
+      children[4].children[0].innerHTML === children[5].children[0].innerHTML &&
+      children[5].children[0].innerHTML ===
+        children[index].children[0].innerHTML
+    ) {
+      isGameStarted = false;
+    }
+  } else {
+    if (
+      children[6].children[0].innerHTML === children[7].children[0].innerHTML &&
+      children[7].children[0].innerHTML === children[8].children[0].innerHTML &&
+      children[8].children[0].innerHTML ===
+        children[index].children[0].innerHTML
+    ) {
+      isGameStarted = false;
+    }
+  }
+
+  //Check Column
+  if (
+    children[0].children[0].innerHTML === children[3].children[0].innerHTML &&
+    children[3].children[0].innerHTML === children[6].children[0].innerHTML &&
+    children[6].children[0].innerHTML === children[index].children[0].innerHTML
+  ) {
+    isGameStarted = false;
+  } else if (
+    children[1].children[0].innerHTML === children[4].children[0].innerHTML &&
+    children[4].children[0].innerHTML === children[7].children[0].innerHTML &&
+    children[7].children[0].innerHTML === children[index].children[0].innerHTML
+  ) {
+    isGameStarted = false;
+  } else if (
+    children[2].children[0].innerHTML === children[5].children[0].innerHTML &&
+    children[5].children[0].innerHTML === children[8].children[0].innerHTML &&
+    children[8].children[0].innerHTML === children[index].children[0].innerHTML
+  ) {
+    isGameStarted = false;
+  }
+
+  //Check Diagonal
+  if (
+    children[0].children[0].innerHTML === children[4].children[0].innerHTML &&
+    children[4].children[0].innerHTML === children[8].children[0].innerHTML &&
+    children[8].children[0].innerHTML === children[index].children[0].innerHTML
+  ) {
+    isGameStarted = false;
+  } else if (
+    children[2].children[0].innerHTML === children[4].children[0].innerHTML &&
+    children[4].children[0].innerHTML === children[6].children[0].innerHTML &&
+    children[6].children[0].innerHTML === children[index].children[0].innerHTML
+  ) {
+    isGameStarted = false;
+  }
+
+  if (!isGameStarted) {
+    swal({ title: `Ganó el jugador ${currentPlayer}`, icon: 'success' });
+  } else if (currentPlayer === 1) {
+    currentPlayer++;
+  } else if (currentPlayer === 2) {
+    currentPlayer--;
+  }
+}
+
+function playerClick(cell, index) {
+  let value = cell.children[0].innerHTML;
+  if (isGameStarted && value === '') {
+    movesPlayed++;
+    if (currentPlayer === 1) {
+      cell.children[0].innerHTML = 'X';
+      checkMove(index);
+      valoresTablero[index] = 1;
+      setTimeout(() => {
+        predecirJugada();
+      }, 100);
+    } else if (currentPlayer === 2) {
+      cell.children[0].innerHTML = 'O';
+      checkMove(index);
+      valoresTablero[index] = -1;
+    }
+
+    if (movesPlayed === 9 && isGameStarted) {
+      swal({ title: 'Empate', icon: 'success' });
+      isGameStarted = false;
+    }
+  }
+}
+
+function setEventListeners() {
+  for (let i = 0; i < tablero.children.length; i++) {
+    let cell = tablero.children[i];
+    cell.addEventListener('click', function () {
+      playerClick(this, i);
+    });
+  }
+}
+
+function setCellStyles() {
+  const borderSize = 7;
+  let borderStyle1 = `border-width: 0 ${borderSize}px ${borderSize}px 0`;
+  let borderStyle2 = `border-width: 0 0 ${borderSize}px 0`;
+  let borderStyle3 = `border-width: 0 ${borderSize}px 0 0`;
+
+  for (let i = 0; i < tablero.children.length; i++) {
+    switch (i) {
+      case 0:
+        tablero.children[i].style = borderStyle1;
+        break;
+      case 1:
+        tablero.children[i].style = borderStyle1;
+        break;
+      case 2:
+        tablero.children[i].style = borderStyle2;
+        break;
+      case 3:
+        tablero.children[i].style = borderStyle1;
+        break;
+      case 4:
+        tablero.children[i].style = borderStyle1;
+        break;
+      case 5:
+        tablero.children[i].style = borderStyle2;
+        break;
+      case 6:
+        tablero.children[i].style = borderStyle3;
+        break;
+      case 7:
+        tablero.children[i].style = borderStyle3;
+        break;
+    }
+  }
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  setCells();
+  setCellStyles();
+  setEventListeners();
+  cargarModelo();
+  // boton reiniciar
+
+  restartButton.addEventListener('click', function () {
+    restartGame();
+  });
+
+  //boton predecir
+  btnPredecir.addEventListener('click', function () {
+    // console.log(valoresTablero);
+  });
+});
+
+const cargarModelo = () => {
+  tf.ready().then(() => {
+    const modelPath = './model/ttt_model.json';
+    tf.tidy(() => {
+      tf.loadLayersModel(modelPath).then((model) => {
+        modelo = model;
+      });
+    });
+  });
+};
+
+const predecirJugada = () => {
+  const valoresX = tf.tensor(valoresTablero);
+
+  const matches = tf.stack([valoresX]);
+
+  const result = modelo.predict(matches);
+
+  const resultArray = result.arraySync();
+  const elementoMayor = result.max().arraySync();
+  const index = resultArray[0].indexOf(elementoMayor);
+
+  console.log(resultArray);
+  console.log(elementoMayor);
+  console.log(index);
+
+  // encontrar la celda que corresponde al indice
+
+  const cell = tablero.children[index];
+  playerClick(cell, index);
+};
